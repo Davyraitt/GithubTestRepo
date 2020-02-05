0c870e7468d7fe634bccb236d7359da35ed110a3
Davy <dj.raitt@student.avans.nl>
2020-02-05 12:47:51 +0100

    Nog een commit, via gitkraken
    Nog een commit, via gitkraken

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("ik voeg nog effe een test toe :)");
+        System.out.println("Hoi Robin");
     }
 
 }
